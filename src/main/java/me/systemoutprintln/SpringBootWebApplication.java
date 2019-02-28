7bdefefd189308e48aa0f3821922b39978c96124
systemoutprintln7 <eric_solis07@hotmail.it>
2019-02-28 19:40:31 +0100

    Added a testing form handling

--- src/main/java/me/systemoutprintln/SpringBootWebApplication.java
+++ src/main/java/me/systemoutprintln/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package me.systemoutprintln.SpringBootWeb;
+package me.systemoutprintln;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
